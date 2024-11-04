b6e96e6e7658805c424cb85b106dd059da75f8ca
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2024-11-04 18:14:24 -0300

    ajustando cors

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import dotenv from 'dotenv';
-import { resolve } from 'path';
-if (process.env.NODE_ENV !== 'production') {
-  // Carrega dotenv apenas em desenvolvimento
+// import dotenv from 'dotenv';
+// import { resolve } from 'path';
+// if (process.env.NODE_ENV !== 'production') {
+//   // Carrega dotenv apenas em desenvolvimento
  
-  dotenv.config({ path: resolve(__dirname, '.env.local') });
-}
+//   dotenv.config({ path: resolve(__dirname, '.env.local') });
+// }
 
-console.log('VITE_API_URL:', process.env.VITE_API_URL);
 
 export default defineConfig({
   plugins: [react(), svgr()],
